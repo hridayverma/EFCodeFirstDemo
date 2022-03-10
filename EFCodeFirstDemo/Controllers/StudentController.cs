76bca9e30c3dc0cffbd3c82ea67e75285c9db57d
hridayverma <hrid.06@gmail.com>
2022-03-10 14:17:21 +0530

    Add project files.

---
+++ EFCodeFirstDemo/Controllers/StudentController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFCodeFirstDemo.Models;
+
+namespace EFCodeFirstDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        PGDACContext db = new PGDACContext();//creating DBCotext object for db opertaions
+        // GET: Student
+        public ActionResult Index()
+        {
+            //Select * from table
+            //return View(db.Students.ToList());
+            return View(db.Students.Include("Address").ToList());
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        //Insert Code to save student data
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);//adding current entity in context
+                db.SaveChanges();//saving in database
+                //ViewBag.Message = "Student Data saved Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(student);
+
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EFCodeFirstDemo/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EFCodeFirstDemo.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }  //scaler property
+        public string  StudentName { get; set; }//scaler property
+        public string DateOfBirth { get; set; }//scaler property
+        public Address Address { get; set; }  //navigation property
+    }
+    public class Address {
+
+        public int Id { get; set; }
+        public string AddressLine { get; set; }
+        public string City { get; set; }
+        public ICollection<Student> Students { get; set; }
+
+    }
+}
